e3a21f2c7826cd773280132bfb5b08a0ec0eb4fd
Matt Glover <matt@duchysoftware.com>
2018-05-30 21:54:34 +0100

    Added public init to DaysToGoService class

--- DaysToGoKit/Services/DaysToGoService.swift
+++ DaysToGoKit/Services/DaysToGoService.swift
@@ -2,10 +2,12 @@ import Foundation
 
 public class DaysToGoService : DaysToGoServiceProtocol {
 
+	public init() {
+
+	}
+
 	public func createNewEvent(date: Date, title: String) -> Event {
 		let event = DaysToGoEvent(date: date, title: title)
 		return event
 	}
-
-	
 }
