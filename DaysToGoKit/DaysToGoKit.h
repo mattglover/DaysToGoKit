574cebb55b4b0fab96709c60e073a8da33d1e776
Matt Glover <matt@duchysoftware.com>
2018-05-30 20:32:44 +0100

    Initial Commit

---
+++ DaysToGoKit/DaysToGoKit.h
@@ -0,0 +1,19 @@
+//
+//  DaysToGoKit.h
+//  DaysToGoKit
+//
+//  Created by Matt Glover on 30/05/2018.
+//  Copyright © 2018 Matt Glover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DaysToGoKit.
+FOUNDATION_EXPORT double DaysToGoKitVersionNumber;
+
+//! Project version string for DaysToGoKit.
+FOUNDATION_EXPORT const unsigned char DaysToGoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DaysToGoKit/PublicHeader.h>
+
+
